e70bba880e2bcca49fa2687ca7e6feb413ad9d09
savelmtr <savelmtr@gmail.com>
2021-01-08 16:58:03 +0300

    настроено автоматическое использование webp

--- website/search/services.py
+++ website/search/services.py
@@ -3,6 +3,7 @@ from wagtail.search.models import Query
 from wagtail.core.models import Site
 from blog.models import BlogPage
 
+
 def make_search_query(search_query, request):
     # Search
     if search_query:
@@ -12,7 +13,6 @@ def make_search_query(search_query, request):
             .search(search_query)
         )
         query = Query.get(search_query)
-
         # Record hit
         query.add_hit()
     else:
@@ -23,11 +23,14 @@ def make_search_query(search_query, request):
 
 def get_pages_by_tags(tags, request):
     if tags:
-        results = (
-            BlogPage.objects.live()
-            .descendant_of(Site.find_for_request(request).root_page)
-            .filter(tags__slug__in=tags)
-        )
+        q = []
+        for t in tags:
+            q.append(
+                BlogPage.objects.live()
+                .descendant_of(Site.find_for_request(request).root_page)
+                .filter(tags__slug=t)
+            )
+        results = q[0].intersection(*q[1:])
     else:
         results = BlogPage.objects.none()
 
